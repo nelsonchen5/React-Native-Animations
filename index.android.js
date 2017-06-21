a838e632d5e82266772eb1fc3d5c69eecef45ba1
jason <jason@codedaily.io>
2017-06-21 08:59:04 -0700

    Add spring example

--- index.android.js
+++ index.android.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Animated, TouchableWithoutFeedback } from "react-native";
 
 export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(1)
+  };
+
+  handlePress = () => {
+    Animated.spring(this.state.animation, {
+      toValue: 2,
+      friction: 2,
+      tension: 160
+    }).start(() => {
+      Animated.timing(this.state.animation, {
+        toValue: 1,
+        duration: 100
+      }).start();
+    });
+  }
+
   render() {
+    const animatedStyle = {
+      transform: [
+        { scale: this.state.animation}
+      ]
+    }
     return (
       <View style={styles.container}>
-      
+        <TouchableWithoutFeedback onPress={this.handlePress}>
+          <Animated.View style={[styles.box, animatedStyle]} />
+        </TouchableWithoutFeedback>
       </View>
     );
 
@@ -15,7 +39,14 @@ export default class animations extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: "tomato",
+  }
 });
 
 AppRegistry.registerComponent("animations", () => animations);
