88384e584365d4b48ea00f2d3b670c1875093b80
jason <jason@codedaily.io>
2017-06-21 09:43:35 -0700

    Add example

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,7 @@ export default class animations extends Component {
   };
   startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: 300,
+      toValue: 1,
       duration: 1500
     }).start(() => {
       this.state.animation.setValue(0);
@@ -15,15 +15,30 @@ export default class animations extends Component {
   }
   
   render() {
-    const animatedStyles = {
-      transform: [
-        { translateY: this.state.animation }
-      ]
+    const boxInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
+    });
+
+    const colorInterpolation = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["rgb(99,71,255)", "rgb(255,99,71)" ]
+    });
+
+    const boxAnimatedStyles = {
+      backgroundColor: boxInterpolation
+    };
+
+    const textAnimatedStyles = {
+      color: colorInterpolation
     }
+    
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
-          <Animated.View style={[styles.box, animatedStyles]} />
+          <Animated.View style={[styles.box, boxAnimatedStyles]}>
+            <Animated.Text style={textAnimatedStyles}>Hello Animation!</Animated.Text>
+          </Animated.View>
         </TouchableWithoutFeedback>
       </View>
     );
@@ -40,7 +55,8 @@ const styles = StyleSheet.create({
   box: {
     width: 150,
     height: 150,
-    backgroundColor: "tomato",
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
 
