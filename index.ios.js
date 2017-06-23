521b662643e770aff206e648b292c31affdf9829
jason <jason@codedaily.io>
2017-06-23 11:20:14 -0700

    Add basic example

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,60 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+import { AppRegistry, StyleSheet, Text, View, Animated, ScrollView, TouchableOpacity } from "react-native";
 
 export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
+  }
+  _enabled = true;
+
+  handleToggle = () => {
+    this._enabled = !this._enabled;
+    let style = [styles.scroll];
+
+    if (!this._enabled) {
+      style.push(styles.hide);
+    } else {
+      // style.push(styles.show)
+    }
+
+    this._scroll.setNativeProps({
+      scrollEnabled: this._enabled,
+      style
+    });
+  }
+
   render() {
+
+    const bgInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 3000],
+      outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
+    });
+    const scrollStyle = {
+      backgroundColor: bgInterpolate
+    }
     return (
       <View style={styles.container}>
-      
+        <TouchableOpacity onPress={this.handleToggle}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+
+        <ScrollView 
+          style={styles.scroll}
+          ref={(scroll) => this._scroll = scroll}
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  y: this.state.animation,
+                }
+              }
+            }
+          ])}
+        >
+          <Animated.View style={[styles.fakeContent, scrollStyle]} />
+        </ScrollView>
+
       </View>
     );
 
@@ -15,7 +64,22 @@ export default class animations extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 50,
   },
+  scroll: {
+    flex: 1,
+    // opacity: 1,
+  },
+  hide: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+  },
+  fakeContent: {
+    height: 3000,
+    backgroundColor: "tomato",
+  }
 });
 
 AppRegistry.registerComponent("animations", () => animations);
