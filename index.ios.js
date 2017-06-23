27ae7840bf898b3d17c4bcc037b8eab65afa1107
jason <jason@codedaily.io>
2017-06-23 12:28:51 -0700

    Add in code

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,65 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+} from "react-native";
+import Svg, { Path } from "react-native-svg";
+import { interpolatePath } from "d3-interpolate-path";
+
+
+const startPath = `M45,50a5,5 0 1,0 10,0a5,5 0 1,0 -10,0`
+const endPath = `M20,50a30,30 0 1,0 60,0a30,30 0 1,0 -60,0`
 
 export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
+  };
+
+  handlePress = () => {
+    const pathInterpolate = interpolatePath(startPath, endPath);
+
+    this.state.animation.addListener(({ value }) => {
+      const path = pathInterpolate(value);
+      this._path.setNativeProps({
+        d: path,
+      });
+    });
+
+    Animated.sequence([
+      Animated.timing(this.state.animation, {
+        toValue: 1,
+        duration: 500,
+      }),
+      Animated.delay(1500),
+      Animated.timing(this.state.animation, {
+        toValue: 0,
+        duration: 500
+      })
+    ]).start();
+    
+  };
   render() {
     return (
       <View style={styles.container}>
-      
+        <TouchableWithoutFeedback onPress={this.handlePress}>
+          <Svg width={150} height={150}>
+            <Path d={startPath} stroke="black" ref={path => this._path = path} />
+          </Svg>
+        </TouchableWithoutFeedback>
       </View>
     );
-
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
