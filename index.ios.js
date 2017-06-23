063de7373edb805632be05afff8be95be88a9525
jason <jason@codedaily.io>
2017-06-23 14:07:04 -0700

    Add code

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+
+import "art/modes/svg.js";
+
 import {
   AppRegistry,
   StyleSheet,
@@ -7,12 +10,15 @@ import {
   Animated,
   TouchableWithoutFeedback,
 } from "react-native";
+
 import Svg, { Path } from "react-native-svg";
-import { interpolatePath } from "d3-interpolate-path";
 
+import SVGPath from "art/modes/svg/path";
+import CorePath from "art/core/path";
+import { Tween } from "art/morph/path";
 
-const startPath = `M45,50a5,5 0 1,0 10,0a5,5 0 1,0 -10,0`
-const endPath = `M20,50a30,30 0 1,0 60,0a30,30 0 1,0 -60,0`
+const startPath = `M32,16.009c0-0.267-0.11-0.522-0.293-0.714  l-9.899-9.999c-0.391-0.395-1.024-0.394-1.414,0c-0.391,0.394-0.391,1.034,0,1.428l8.193,8.275H1c-0.552,0-1,0.452-1,1.01  s0.448,1.01,1,1.01h27.586l-8.192,8.275c-0.391,0.394-0.39,1.034,0,1.428c0.391,0.394,1.024,0.394,1.414,0l9.899-9.999  C31.894,16.534,31.997,16.274,32,16.009z`;
+const endPath = `M27.704,8.397c-0.394-0.391-1.034-0.391-1.428,0  L11.988,22.59l-6.282-6.193c-0.394-0.391-1.034-0.391-1.428,0c-0.394,0.391-0.394,1.024,0,1.414l6.999,6.899  c0.39,0.386,1.039,0.386,1.429,0L27.704,9.811C28.099,9.421,28.099,8.787,27.704,8.397C27.31,8.006,28.099,8.787,27.704,8.397z`;
 
 export default class animations extends Component {
   state = {
@@ -20,13 +26,16 @@ export default class animations extends Component {
   };
 
   handlePress = () => {
-    const pathInterpolate = interpolatePath(startPath, endPath);
+    const pathInterpolate = Tween(startPath, endPath);
+    const p = new SVGPath();
 
     this.state.animation.addListener(({ value }) => {
-      const path = pathInterpolate(value);
+      pathInterpolate.tween(value);
+      pathInterpolate.applyToPath(p);
+
       this._path.setNativeProps({
-        d: path,
-      });
+        d: p.toSVG()
+      })
     });
 
     Animated.sequence([
@@ -37,17 +46,20 @@ export default class animations extends Component {
       Animated.delay(1500),
       Animated.timing(this.state.animation, {
         toValue: 0,
-        duration: 500
-      })
+        duration: 500,
+      }),
     ]).start();
-    
   };
   render() {
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.handlePress}>
           <Svg width={150} height={150}>
-            <Path d={startPath} stroke="black" ref={path => this._path = path} />
+            <Path
+              d={startPath}
+              stroke="black"
+              ref={path => (this._path = path)}
+            />
           </Svg>
         </TouchableWithoutFeedback>
       </View>
