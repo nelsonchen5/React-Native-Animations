238e7b7277d7ebc05ce7c64b0643df1af82e229f
jason <jason@codedaily.io>
2017-07-06 14:35:25 -0700

    Add code

--- index.ios.js
+++ index.ios.js
@@ -3,23 +3,45 @@ import { AppRegistry, StyleSheet, Text, View, Animated, TouchableWithoutFeedback
 
 export default class animations extends Component {
   state = {
-    animation: new Animated.Value(0),
+    animation: new Animated.Value(1),
   };
   startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: 300,
+      // toValue: 3,
+      toValue: 2,
       duration: 1500
     }).start(() => {
-      this.state.animation.setValue(0);
+      Animated.timing(this.state.animation, {
+        // toValue: 0,
+        toValue: 1,
+        duration: 300
+      }).start();
+
+      // this.state.animation.setValue(5);
     });
   }
   
   render() {
+
+    //extend: Default
+    //clamp: Whatever the end values we've defined are the values they will stay at, never go beyond
+    //identity: Takes on the value of the Animated.Value that you're passing in and ignores inputRange/outputRange
+
+    const scaleInterpolate = this.state.animation.interpolate({
+      inputRange: [1, 2],
+      outputRange: [1, 2],
+      // extrapolate: "identity",
+      // extrapolate: "clamp",
+      // extrapolateLeft: "clamp",
+      // extrapolateRight: "clamp"
+    })
+
     const animatedStyles = {
       transform: [
-        { translateY: this.state.animation }
+        { scale: scaleInterpolate }
       ]
     }
+    
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
