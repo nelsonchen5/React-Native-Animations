e1e23d44c67949144f3650accfdc139c1ce1c8ba
jason <jason@codedaily.io>
2017-07-13 11:01:40 -0700

    Add code

---
+++ images.js
@@ -0,0 +1,41 @@
+
+import Screen1Image1 from "./imgs/s1/1.png";
+import Screen1Image2 from "./imgs/s1/2.png";
+import Screen1Image3 from "./imgs/s1/3.png";
+import Screen1Image4 from "./imgs/s1/4.png";
+import Screen1Image5 from "./imgs/s1/5.png";
+
+import Screen2Image1 from "./imgs/s2/1.png";
+import Screen2Image2 from "./imgs/s2/2.png";
+import Screen2Image3 from "./imgs/s2/3.png";
+
+import Screen3Image1 from "./imgs/s3/1.png";
+import Screen3Image2 from "./imgs/s3/2.png";
+import Screen3Image3 from "./imgs/s3/3.png";
+import Screen3Image4 from "./imgs/s3/4.png";
+import Screen3Image5 from "./imgs/s3/5.png";
+
+import Screen4Image1 from "./imgs/s4/1.png";
+import Screen4Image2 from "./imgs/s4/2.png";
+import Screen4Image3 from "./imgs/s4/3.png";
+import Screen4Image4 from "./imgs/s4/4.png";
+
+export {
+  Screen1Image1,
+  Screen1Image2,
+  Screen1Image3,
+  Screen1Image4,
+  Screen1Image5,
+  Screen2Image1,
+  Screen2Image2,
+  Screen2Image3,
+  Screen3Image1,
+  Screen3Image2,
+  Screen3Image3,
+  Screen3Image4,
+  Screen3Image5,
+  Screen4Image1,
+  Screen4Image2,
+  Screen4Image3,
+  Screen4Image4,
+}
--- index.ios.js
+++ index.ios.js
@@ -1,19 +1,268 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  ScrollView,
+  Dimensions,
+  PixelRatio,
+} from "react-native";
+
+import * as Images from "./images";
+
+const getScreen1Styles = (animation, width) => {
+  const image2TranslateX = animation.interpolate({
+    inputRange: [0, width],
+    outputRange: [0, -100],
+    extrapolate: "clamp",
+  });
+
+  return {
+    image2: {
+      transform: [
+        {
+          translateX: image2TranslateX,
+        },
+      ],
+    },
+  };
+};
+
+const getScreen2Styles = (animation, width) => {
+  const inputRange = [-width, width, width * 2];
+
+  const image2TranslateY = animation.interpolate({
+    inputRange,
+    outputRange: [100, 0, -100],
+    extrapolate: "clamp",
+  });
+  const image2Opacity = animation.interpolate({
+    inputRange,
+    outputRange: [0, 1, 0],
+    extrapolate: "clamp",
+  });
+
+  return {
+    image2: {
+      opacity: image2Opacity,
+      transform: [
+        {
+          translateY: image2TranslateY,
+        },
+      ],
+    },
+  };
+};
+
+const getScreen3Styles = (animation, width) => {
+  const inputRange = [-width * 2, width * 2, width * 3];
+
+  const image1Scale = animation.interpolate({
+    inputRange,
+    outputRange: [0, 1, 0],
+    extrapolate: "clamp",
+  });
+
+  const image2Rotate = animation.interpolate({
+    inputRange,
+    outputRange: ["180deg", "0deg", "180deg"],
+    extrapolate: "clamp",
+  });
+
+  return {
+    image1: {
+      transform: [
+        {
+          scale: image1Scale,
+        },
+      ],
+    },
+    image2: {
+      transform: [
+        {
+          scale: image1Scale,
+        },
+        {
+          rotate: image2Rotate,
+        },
+      ],
+    },
+  };
+};
 
 export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
+  };
   render() {
+    const { width, height } = Dimensions.get("window");
+
+    const screen1Interpolations = getScreen1Styles(this.state.animation, width);
+    const screen2Interpolations = getScreen2Styles(this.state.animation, width);
+    const screen3Interpolations = getScreen3Styles(this.state.animation, width);
+
     return (
       <View style={styles.container}>
-      
+        <ScrollView
+          style={styles.container}
+          pagingEnabled
+          horizontal
+          scrollEventThrottle={16}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: this.state.animation,
+                },
+              },
+            },
+          ])}
+        >
+          <View style={{ width, height, backgroundColor: "#F89E20" }}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Screen1Image1}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(75),
+                  height: PixelRatio.getPixelSizeForLayoutSize(63),
+                }}
+                resizeMode="contain"
+              />
+
+              <Animated.Image
+                source={Images.Screen1Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: "absolute",
+                    top: 200,
+                    left: 60,
+                  },
+                  screen1Interpolations.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Screen1Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: "absolute",
+                  top: 150,
+                  left: 60,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 1</Text>
+            </View>
+          </View>
+
+          <View style={{ width, height, backgroundColor: "#F89E20" }}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Screen1Image1}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(75),
+                  height: PixelRatio.getPixelSizeForLayoutSize(63),
+                }}
+                resizeMode="contain"
+              />
+
+              <Animated.Image
+                source={Images.Screen1Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: "absolute",
+                    top: 200,
+                    left: 60,
+                  },
+                  screen2Interpolations.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Screen1Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: "absolute",
+                  top: 150,
+                  left: 60,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 2</Text>
+            </View>
+          </View>
+          <View style={{ width, height, backgroundColor: "#F89E20" }}>
+            <View style={styles.screenHeader}>
+              <Animated.Image
+                source={Images.Screen1Image1}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(75),
+                    height: PixelRatio.getPixelSizeForLayoutSize(63),
+                  },
+                  screen3Interpolations.image1,
+                ]}
+                resizeMode="contain"
+              />
+
+              <Animated.Image
+                source={Images.Screen1Image2}
+                style={[
+                  {
+                    width: PixelRatio.getPixelSizeForLayoutSize(46),
+                    height: PixelRatio.getPixelSizeForLayoutSize(28),
+                    position: "absolute",
+                    top: 200,
+                    left: 60,
+                  },
+                  screen3Interpolations.image2,
+                ]}
+                resizeMode="contain"
+              />
+              <Animated.Image
+                source={Images.Screen1Image3}
+                style={{
+                  width: PixelRatio.getPixelSizeForLayoutSize(23),
+                  height: PixelRatio.getPixelSizeForLayoutSize(17),
+                  position: "absolute",
+                  top: 150,
+                  left: 60,
+                }}
+                resizeMode="contain"
+              />
+            </View>
+            <View style={styles.screenText}>
+              <Text>Screen 3</Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     );
-
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  screenHeader: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  screenText: {
     flex: 1,
   },
 });
