26a7f2ac55b9b237d502beeca10cf1a3e2c6cc00
jason <jason@codedaily.io>
2017-07-14 15:51:36 -0700

    Add code upgrade dynamic dimensions

--- index.android.js
+++ index.android.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated, TouchableWithoutFeedback, Dimensions } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from "react-native";
 
 export default class animations extends Component {
   state = {
@@ -10,32 +18,39 @@ export default class animations extends Component {
 
     Animated.sequence([
       Animated.spring(this.state.animation.y, {
-        toValue: height - 150
+        toValue: height - this._height,
       }),
       Animated.spring(this.state.animation.x, {
-        toValue: width - 150,
+        toValue: width - this._width,
       }),
       Animated.spring(this.state.animation.y, {
-        toValue: 0
+        toValue: 0,
       }),
       Animated.spring(this.state.animation.x, {
-        toValue: 0
-      })
+        toValue: 0,
+      }),
     ]).start();
-  }
-  
+  };
+
+  saveDimensions = e => {
+    this._width = e.nativeEvent.layout.width;
+    this._height = e.nativeEvent.layout.height;
+  };
+
   render() {
     const animatedStyles = {
-      transform: this.state.animation.getTranslateTransform()
-    }
+      transform: this.state.animation.getTranslateTransform(),
+    };
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={this.startAnimation}>
+        <TouchableWithoutFeedback 
+          onPress={this.startAnimation} 
+          onLayout={this.saveDimensions}
+        >
           <Animated.View style={[styles.box, animatedStyles]} />
         </TouchableWithoutFeedback>
       </View>
     );
-
   }
 }
 
@@ -50,7 +65,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 0,
     left: 0,
-  }
+  },
 });
 
 AppRegistry.registerComponent("animations", () => animations);
