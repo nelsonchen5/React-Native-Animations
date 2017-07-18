b3f2bcdc1b0b244c81392413f51299fbe64a8bef
jason <jason@codedaily.io>
2017-07-18 16:41:29 -0700

    Add format

--- index.android.js
+++ index.android.js
@@ -1,33 +1,41 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated, TouchableWithoutFeedback } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default class animations extends Component {
   state = {
     animation: new Animated.Value(0),
-    position: new Animated.Value(0)
+    position: new Animated.Value(0),
   };
   startAnimation = () => {
     Animated.parallel([
       Animated.timing(this.state.animation, {
         toValue: 100,
-        duration: 1500
+        duration: 1500,
       }),
       Animated.timing(this.state.position, {
         toValue: 24,
-        duration: 1500
-      })
+        duration: 1500,
+      }),
     ]).start();
-  }
-  
+  };
+
   render() {
     const randomValue = 3;
-    const newAnimation = Animated.multiply(this.state.position, Animated.modulo(this.state.animation, randomValue))
+    const newAnimation = Animated.multiply(
+      this.state.position,
+      Animated.modulo(this.state.animation, randomValue)
+    );
 
     const animatedStyles = {
-      transform: [
-        { translateY: newAnimation }
-      ]
-    }
+      transform: [{ translateY: newAnimation }],
+    };
     return (
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={this.startAnimation}>
@@ -35,7 +43,6 @@ export default class animations extends Component {
         </TouchableWithoutFeedback>
       </View>
     );
-
   }
 }
 
@@ -49,7 +56,7 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     backgroundColor: "tomato",
-  }
+  },
 });
 
 AppRegistry.registerComponent("animations", () => animations);
