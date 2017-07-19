0140d9102ce759417ea9d3f6ac90f065dadb2175
jason <jason@codedaily.io>
2017-07-18 18:44:30 -0700

    Add format

--- index.ios.js
+++ index.ios.js
@@ -4,37 +4,38 @@ import { AppRegistry, StyleSheet, Text, View, Animated, ScrollView } from "react
 export default class animations extends Component {
   state = {
     animation: new Animated.Value(0),
-  }
+  };
   render() {
-
     const opacityInterpolate = this.state.animation.interpolate({
       inputRange: [0, 3000],
-      outputRange: [1, 0]
+      outputRange: [1, 0],
     });
 
     const backgroundStyle = {
       backgroundColor: "tomato",
-      opacity: opacityInterpolate
-    }
+      opacity: opacityInterpolate,
+    };
     return (
       <View style={styles.container}>
         <Animated.ScrollView
           scrollEventThrottle={1}
-          onScroll={Animated.event([
-            {
-              nativeEvent: {
-                contentOffset: {
-                  y: this.state.animation
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    y: this.state.animation,
+                  },
                 },
-              }
-            }
-          ], { useNativeDriver: true })}
+              },
+            ],
+            { useNativeDriver: true }
+          )}
         >
           <Animated.View style={[styles.content, backgroundStyle]} />
         </Animated.ScrollView>
       </View>
     );
-
   }
 }
 
@@ -44,7 +45,7 @@ const styles = StyleSheet.create({
   },
   content: {
     height: 3000,
-  }
+  },
 });
 
 AppRegistry.registerComponent("animations", () => animations);
