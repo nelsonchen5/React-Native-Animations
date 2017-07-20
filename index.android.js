b38f6b466f0459df8d7a12c9c60f5858d44c0cfe
jason <jason@codedaily.io>
2017-07-19 17:28:18 -0700

    Add code

--- index.android.js
+++ index.android.js
@@ -1,20 +1,75 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
 
 export default class animations extends Component {
+  state = {
+    animation: new Animated.Value(0),
+    toggle: true,
+  };
+  _pressed = false;
+  handleToggle = () => {
+    this.setState(state => ({
+      toggle: !state.toggle,
+    }));
+  };
+  handlePress = () => {
+    const toValue = this._pressed ? 0 : 1;
+    Animated.timing(this.state.animation, {
+      toValue
+    }).start();
+    this._pressed = !this._pressed;
+  };
   render() {
+    
+    const boxStyle = {
+      backgroundColor: this.state.animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["rgb(255,99,71)", "rgb(99,71,255)"]
+      })
+    }
+
     return (
       <View style={styles.container}>
-      
+        <View>
+          <TouchableWithoutFeedback onPress={this.handlePress}>
+            <Animated.View style={[styles.box, boxStyle]} />
+          </TouchableWithoutFeedback>
+          <View
+            style={[StyleSheet.absoluteFill, styles.cover]}
+            pointerEvents={this.state.toggle ? "none" : "auto"}
+          />
+        </View>
+
+        <TouchableOpacity onPress={this.handleToggle}>
+          <View>
+            <Text>Toggle Pointer Events</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
-
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  cover: {
+    backgroundColor: "transparent",
   },
 });
 
